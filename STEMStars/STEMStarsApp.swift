483fdf2c6884df5c03f2c8b066e114eebfc5e116
KWK <kwk4.52024@gmail.com>
2024-07-25 14:40:48 -0700

    Initial Commit

---
+++ STEMStars/STEMStarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  STEMStarsApp.swift
+//  STEMStars
+//
+//  Created by Scholar on 7/24/24.
+//
+
+import SwiftUI
+
+@main
+struct STEMStarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
